bed941868b52c558b0a550f96a54b72731c41455
sarvkk <sarvajitkhadka444@gmail.com>
2026-08-25 00:38:17 +0545

    Add Vercel adapter entrypoint for the API

---
+++ apps/api/api/index.ts
@@ -0,0 +1,7 @@
+import { handle } from "hono/vercel";
+import app from "../src/index";
+
+export const GET = handle(app);
+export const POST = handle(app);
+export const DELETE = handle(app);
+export const OPTIONS = handle(app);
